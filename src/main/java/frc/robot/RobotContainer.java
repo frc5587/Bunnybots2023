e7a5134be339cdd2477894649842599844aefc4c
titanrobotics5587 <tcwilliamsrobotics@gmail.com>
2024-01-27 16:37:58 -0500

    limelight stuff!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,13 +13,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.commands.Auto;
 import frc.robot.commands.BottomAll;
 import frc.robot.commands.DualStickSwerve;
 import frc.robot.commands.TopAll;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.Wrist;
 
@@ -34,8 +34,9 @@ import frc.robot.subsystems.Wrist;
  */
 public class RobotContainer {
     // SUBSYSTEMS:
+    private final Limelight limelight = new Limelight();
     private final Elevator elevator = new Elevator();
-    protected final Swerve swerve = new Swerve();
+    protected final Swerve swerve = new Swerve(limelight);
     private final Intake intake = new Intake();
     private final Wrist wrist = new Wrist();
 
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -4,6 +4,7 @@ import org.frc5587.lib.subsystems.SwerveBase;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import com.ctre.phoenix6.hardware.CANcoder;
@@ -12,6 +13,7 @@ import com.revrobotics.CANSparkMax;
 import frc.robot.Constants.DrivetrainConstants;
 
 public class Swerve extends SwerveBase {
+    private boolean hasHadTarget = false;
     private static SwerveModule[] swerveModules = {
             new SwerveModule(DrivetrainConstants.Mod0.MODULE_CONSTANTS, new CANSparkMax(10, MotorType.kBrushless),
                     new CANSparkMax(15, MotorType.kBrushless), new CANcoder(50)),
@@ -23,9 +25,26 @@ public class Swerve extends SwerveBase {
                     new CANSparkMax(18, MotorType.kBrushless), new CANcoder(53))
     };
 
-    public Swerve() {
+    private Limelight limelight;
+
+    public Swerve(Limelight limelight) {
         super(DrivetrainConstants.SWERVE_CONSTANTS, swerveModules);
+        this.limelight = limelight;
+        if(limelight.hasTarget()) {
+            resetOdometry(limelight.getLimelightPose());
+            gyro.setYawZeroOffset(gyro.getUnZeroedYaw().plus(limelight.getLimelightPose().getRotation()));
+            // gyro.setYawZeroOffset(new Rotation2d());
+            hasHadTarget = true;
+        }
+        // 15 / 16
+        // 1 / 16 = 1 / 8 / 2
+        // 1 / 8 = 0.125 / 2 = 0.0025 + 0.01 + 0.05 = 0.0625
+        // 16/ 16 = 1.0000 - 0.0625 =~ 0.9465
+        // 0.9375
+        // 0.0625
+
     }
+
 
     @Override
     public void periodic() {
@@ -41,9 +60,17 @@ public class Swerve extends SwerveBase {
         SmartDashboard.putBoolean("Zero Yaw", false);
 
         for (int i = 0; i < swerveModules.length; i++) {
-                SmartDashboard.putNumber("mod "+i+"degrees", swerveModules[i].getRawAbsoluteEncoderValue().getDegrees());
-                SmartDashboard.putNumber("Adjusted absolute "+i, swerveModules[i].getAbsoluteEncoderValue().getDegrees());
-                SmartDashboard.putNumber("Motor " +i, swerveModules[i].getAngle().getDegrees());
-            }
+            SmartDashboard.putNumber("mod "+i+"degrees", swerveModules[i].getRawAbsoluteEncoderValue().getDegrees());
+            SmartDashboard.putNumber("Adjusted absolute "+i, swerveModules[i].getAbsoluteEncoderValue().getDegrees());
+            SmartDashboard.putNumber("Motor " +i, swerveModules[i].getAngle().getDegrees());
+        }
+        if(limelight.hasTarget() && limelight.getTargetSpacePose().getX() <= 1.5) { // if the target is super close, we can set the pose to the limelight pose
+            resetOdometry(limelight.getLimelightPose());
+        }
+        if(limelight.hasTarget()) {
+            poseEstimator.addVisionMeasurement(getEstimatedPose(), 0);
+            poseEstimator.update(getYaw(), getModulePositions());
+        }
+        SmartDashboard.putData("field", field);
     }
 }
